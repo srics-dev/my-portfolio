c6b494255c093f6097625a2ed57983dd247df324
Shrikanth m <104542005+srics-dev@users.noreply.github.com>
2025-04-13 12:08:06 +0530

    correct

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -1,4 +1,4 @@
-"use client"
+{/*"use client"
 
 import * as React from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
@@ -64,3 +64,4 @@ function Calendar({
 Calendar.displayName = "Calendar"
 
 export { Calendar }
+*/}
